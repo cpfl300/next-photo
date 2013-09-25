56fbab6ecc05ebfe10a70f4c1ee7083b33a4ff4e
cpfl300 <cpfl300@naver.com>
2013-09-25 18:41:36 +0900

    파일 첨부 추가

--- src/main/java/org/nhnnext/web/HomeController.java
+++ src/main/java/org/nhnnext/web/HomeController.java
@@ -1,24 +1,31 @@
 package org.nhnnext.web;
 
+import java.io.File;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/board")
+
 public class HomeController {
 	@RequestMapping("/")
 	public String index() {
 		return "index";
 	}
 	
-	@RequestMapping("/board/upload")
+	@RequestMapping("/upload")
 	public String upload() {
 		return "upload";
 	}
 
-	@RequestMapping(value="/board", method=RequestMethod.POST)
-	public String uploaded(String title, String comment) {
-		System.out.println("title: " + title +", comment: " + comment);
-		return "upload";
+	@RequestMapping(value="", method=RequestMethod.POST)
+	public String uploaded(String title, String comment, String photo) {
+		System.out.println("title: " + title +", comment: " + comment+", photo: "+photo);
+	
+		
+		System.out.println("");
+		return "redirect:/";
 	}
 }
--- webapp/upload.jsp
+++ webapp/upload.jsp
@@ -5,6 +5,13 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
+
+<style type="text/css">
+	h3, textarea{color: skyblue;}
+	h3{font-family: sans-serif;
+		text-decoration: overline;}
+</style>
+
 </head>
 <body>
 	<h3>사진 업로드하기</h3>
@@ -13,6 +20,7 @@
 		<label for="title"> photo title: </label>
 		<textarea rows="1" cols="30" name="title"></textarea><br>
 		<textarea rows="10" cols="45" name="comment">photo comment</textarea><br>
+		<input type="file" value="파일 선택" name="photo"><br>
 		<input type="submit" value="사진올리기">
 	</form>
 	
